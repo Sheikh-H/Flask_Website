1eaccf690ee2d7793711f8980bdea153a98c88b2
Sheikh Hussain <sheikh.hussain1155@gmail.com>
2026-01-18 22:03:56 +0000

    student pages added

--- app.py
+++ app.py
@@ -9,10 +9,14 @@ from python_functions.text_analyser_functions import *
 
 load_dotenv()
 
-URI = os.environ["MONGO_URI"].strip()
+#URI = os.environ["MONGO_URI"].strip()
+URI = os.environ.get("MONGO_URI")
+
 
 client = pymongo.MongoClient(URI, tls=True, tlsCAFile=certifi.where())
 db = client['flask_project']
+students_tbl = db.students
+feedbacks_tbl = db.feedbacks
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -52,17 +56,45 @@ def mongodb_home():
     title = "Using APIs"
     return render_template('/pages/database_home.html', title = title)
 
-@app.route('/API/v1.0/student', methods=['GET'])
+@app.route('/MongoDB/api/v1.0/student', methods=['GET'])
 def student_api():
-    data = list(db.students.find())
-    # for student in data:
-    #     student['_id'] = str(student['_id'])
-    return jsonify(data)
+    students = list(db.students.find())
+    for student in students:
+        student['_id'] = str(student['_id'])
+    return jsonify(students)
 
-@app.route('/API/v1.0/testmonial')
+@app.route('/API/v1.0/testmonial', methods=['GET'])
 def testimonial_api():
-    title = "Testimonial API"
-    return jsonify
+    testimonials = list(db.feedbacks.find())
+    for testimonial in testimonials:
+        testimonial['_id'] = str(testimonial['_id'])
+    return jsonify(testimonials)
+
+
+@app.route('/student_database', methods = ['GET', 'POST'])
+def student_database():
+    title = "Student Database"
+    students = db.students.find({},{'_id':1, 'name':1, 'age':1, 'skills':1, 'created_at': 1})
+    return render_template('/pages/student_database_home.html', title = title, students = students)
+
+@app.route('/add_student', methods = ['GET', 'POST'])
+def add_student():
+    title = "Add new student"
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        skills = request.form['skills'].split(', ')
+        created_at = datetime.now()
+        student = {
+            'name':name,
+            'age': age, 
+            'skills': skills,
+            'created_at':created_at,
+        }
+        result = students_tbl.insert_one(student)
+        redirect(url_for('student_database', student_id=str(result.inserted_id)))
+    return render_template('/pages/add_new_student.html', title = title)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
